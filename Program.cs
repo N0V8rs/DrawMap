f18f4f11ecc53fee7a7cbacc1ada71d5e71ea150
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2023-11-30 16:23:41 -0400

    Doing big changes So lets see if it will break

--- Program.cs
+++ Program.cs
@@ -1,136 +1,182 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Diagnostics.SymbolStore;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace DrawMap
 {
     internal class Program
     {
-        static string path = @"TextFile1.txt"; // Map Name
-        static char[,] dungeonMap; // Map Arrays
+        //Player
+        static int maxPlayerHP;
+        static int playerHP;
+        static int playerDamage;
+        static int playerPosX;
+        static int playerPosY;
+        static ConsoleKeyInfo playerMoveSet;
 
-        static int playerX;
-        static int playerY;
-        static char player = ((char)248);
+        //Enemies
+        static int maxEnemy1HP;
+        static int MaxEnemy2HP;
+        static int enemy1HP;
+        static int enemy2HP;
+        static int enemy1Damage;
+        static int enemy2Damage;
+        static int enemy1PosX;
+        static int enemy1PosY;
+        static int enemy2PosX;
+        static int enemy2PosY;
+        static bool enemy1Alive;
+
+        //Map
+        static string path;
+        static string Map = @"TextFile1.txt";
+        static string[] MapArea;
+        static char[,] MapLayout;
+        static bool gameOver;
+        static bool levelComplete;
+        static int Diamonds;
+        static int mapX;
+        static int mapY;
+        static int maxX;
+        static int maxY;
 
         static void Main(string[] args)
         {
-            LoadMap();
-            DrawMap();
-            while (true)
+
+        }
+
+        static void SartUp()
+        {
+            //Health
+            maxPlayerHP = 10;
+            playerHP = maxPlayerHP;
+            maxEnemy1HP = 3;
+            enemy1HP = maxEnemy1HP;
+
+            //Damage
+            playerDamage = 1;
+            enemy1Damage = 1;
+            enemy2Damage = 1;
+            //Game Systems
+            gameOver = false;
+            levelComplete = false;
+            Diamonds = 0;
+            //Map Load up
+            path = Map;
+            MapArea = File.ReadAllLines(path);
+            MapLayout = new char[MapArea.Length, MapArea[0].Length];
+            // DrawMap method
+
+            maxX = mapX - 1; maxY = mapY - 1;
+        }
+
+        static void DrawMap()
+        {
+            for (int a = 0; a < MapArea.Length; a++)
             {
-                PlayerCharacter();
+                for (int b = 0; b < MapArea[a].Length; b++)
+                {
+                    MapLayout[a,b] = MapArea[a][b];
+                }
             }
         }
 
-        static void LoadMap()
+        static void MapCreated()
         {
-            string[] floorMap = File.ReadAllLines(path);
-
-            // Assuming all lines have the same length
-            int mapY = floorMap.Length;
-            int mapX = floorMap[0].Length;
-
-            dungeonMap = new char[mapY, mapX];
+            Console.Clear();
 
             for (int y = 0; y < mapY; y++)
             {
                 for (int x = 0; x < mapX; x++)
                 {
-                    playerX = x -35;
-                    playerY = y -1;
-                    dungeonMap[y, x] = floorMap[y][x];
-                }
-            }
+                    char tile = MapLayout[y,x];
 
-            SetPlayerPosition();
-            Console.SetCursorPosition(playerX, playerY); // Move this line to SetPlayerPosition
-        }
-
-        static void DrawMap()
-        {
-            // Draws the map of the current level
-            Console.SetCursorPosition(0, 0);
-            for (int y = 0; y < dungeonMap.GetLength(0); y++)
-            {
-                for (int x = 0; x < dungeonMap.GetLength(1); x++)
-                {
-                    char tile = dungeonMap[y, x];
+                    if (tile == '+' && levelComplete == false)
+                    {
+                        playerPosX = x;
+                        playerPosY = y;
+                        levelComplete = true;
+                        MapLayout[y,x] = '#';
+                    }
+                    //'E' = Enemy 1
+                    if (tile == 'E' && levelComplete == false)
+                    {
+                        if (tile == 'E')
+                        {
+                            enemy1PosX = x;
+                            enemy1PosY = y;
+                        }
+                    }
                     Console.Write(tile);
                 }
                 Console.WriteLine();
             }
+            //playerpostion
+            //enemyposition
+            Console.SetCursorPosition(0, 0);
         }
 
-        static void PlayerCharacter()
+        static void PlayerPos()
         {
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
-            // Player position after movement
-            int newPlayerX = playerX;
-            int newPlayerY = playerY;
-
-            // Player Controls
-            switch (keyInfo.Key)
-            {
-                case ConsoleKey.W:
-                case ConsoleKey.UpArrow:
-                    newPlayerY = playerY - 1;
-                    break;
-                case ConsoleKey.S:
-                case ConsoleKey.DownArrow:
-                    newPlayerY = playerY + 1;
-                    break;
-                case ConsoleKey.A:
-                case ConsoleKey.LeftArrow:
-                    newPlayerX = playerX - 1;
-                    break;
-                case ConsoleKey.D:
-                case ConsoleKey.RightArrow:
-                    newPlayerX = playerX + 1;
-                    break;
-            }
-
-            // Check if the new position is valid (within the map boundaries and not a wall)
-            if (IsValidMove(newPlayerX, newPlayerY))
-            {
-                // Erase the player from the current position
-                Console.SetCursorPosition(playerX, playerY);
-                Console.Write(dungeonMap[playerY, playerX]);
-
-                // Update player position
-                playerX = newPlayerX;
-                playerY = newPlayerY;
-
-                // Draw the player at the new position
-                SetPlayerPosition();
-            }
-        }
-
-        static bool IsValidMove(int x, int y)
-        {
-            // Check if the new position is within the map boundaries
-            if (x < 0 || x >= dungeonMap.GetLength(1) || y < 0 || y >= dungeonMap.GetLength(0))
-            {
-                return false;
-            }
-
-            // Check if the new position is not a wall (assuming walls are represented by '#')
-            return dungeonMap[y, x] != '#';
-        }
-
-        static void SetPlayerPosition()
-        {
-            Console.SetCursorPosition(playerX, playerY);
-            DrawPlayer();
-        }
-
-        static void DrawPlayer()
-        {
-            // Used to draw the player
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.BackgroundColor = ConsoleColor.Gray;
-            Console.Write(player);
+            Console.SetCursorPosition(playerPosX, playerPosY);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("!");
             Console.ResetColor();
+        }
+
+        static void PlayerMovement()
+        {
+            bool movement = false;
+            int playerMoveX = playerPosX;
+            int playerMoveY = playerPosY;
+            int newPlayerPostX = 0;
+            int newPlayerPostY = 0;
+            movement = false;
+
+            playerMoveSet = Console.ReadKey(true);
+
+            if (movement == false)
+            {
+                if (playerMoveSet.Key == ConsoleKey.Spacebar)
+                {
+                    //attackEnemy method
+                    movement = true; // Doesn't move when player attacks
+                    return;
+                }
+            }
+
+            if (playerMoveSet.Key != ConsoleKey.W || playerMoveSet.Key == ConsoleKey.UpArrow) 
+            {
+                playerMoveY = Math.Max(playerMoveY -1, 0);
+
+                if (playerMoveY < 0)
+                {
+                    playerMoveY = 0;
+                }
+                if (playerMoveY == enemy1PosY && playerMoveX == enemy1PosX)
+                {
+                    enemy1HP -= 1;
+                    if (enemy1HP <= 0)
+                    {
+                        enemy1PosX = 0;
+                        enemy1PosY = 0;
+                        enemy1Alive = false;
+                    }
+                    return;
+                }
+
+                if (MapLayout[playerMoveY, playerPosY] == '^')
+                {
+                    playerHP -= -1;
+                    if ((playerHP <= 0))
+                    {
+                        gameOver = true;
+                    }
+                }
+            }
+
         }
     }
 }
